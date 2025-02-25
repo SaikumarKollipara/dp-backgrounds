0281febf5116be406f5a74cb603c148442520b53
Saikumar Kollipara <krksaikumar@gmail.com>
2025-02-25 19:23:10 +0530

    updated loader

--- src/components/loading-screen/loading-screen.tsx
+++ src/components/loading-screen/loading-screen.tsx
@@ -2,7 +2,6 @@
 
 import { useHomeStore } from "@/store/home-store";
 import React, { useEffect, useState } from "react";
-import styles from "./loading-screen.module.css";
 
 const LOADING_TEXTS = [
   "Loading...",
@@ -16,22 +15,28 @@ export default function LoadingScreen() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const isLoading = useHomeStore((state) => state.isLoading);
 
-  useEffect(() => {
-    if (!isLoading) return;
+  // useEffect(() => {
+  //   if (!isLoading) return;
 
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % LOADING_TEXTS.length);
-    }, 3000);
+  //   const interval = setInterval(() => {
+  //     setCurrentIndex((prevIndex) => (prevIndex + 1) % LOADING_TEXTS.length);
+  //   }, 3000);
 
-    return () => clearInterval(interval);
-  }, [isLoading]);
+  //   return () => clearInterval(interval);
+  // }, [isLoading]);
 
   if (!isLoading) return null;
 
   return (
-    <div className="fixed inset-0 z-[999] flex flex-col items-center justify-center gap-12 bg-background">
-      <div className={styles.loader}></div>
-      <p>{LOADING_TEXTS[currentIndex]}</p>
+    <div className="fixed left-0 top-0 z-[999] grid h-[100vh] w-[100vw] place-content-center bg-black/80">
+      <div className="flex h-[150px] w-[150px] flex-col items-center justify-center gap-6 rounded-sm bg-white">
+        <img
+          className="h-8 w-8"
+          src="https://upload.wikimedia.org/wikipedia/commons/3/36/Lightness_rotate_36f-L_cw.gif"
+        />
+        {/* <p>{LOADING_TEXTS[currentIndex]}</p> */}
+        <p>Loading...</p>
+      </div>
     </div>
   );
 }
